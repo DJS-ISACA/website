ddccbbd30ef8e551a10eb4e940b42b6b8d7866ca
ManasPatil0967 <manaspatil0967@gmail.com>
2023-03-01 16:03:07 +0530

    "Details in index.js"

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <>
       <Navbar/>
+      <section className="flex justify-between items-center">
+      <p class="flex-1 text-6xl font-black tracking-widest leading-10 text-center text-white">DJS</p>
+    <img class="flex-1 h-1/5 rounded-lg" src="https://via.placeholder.com/248.24896240234375x107"/>
+    <p class="flex-1 text-6xl font-black tracking-widest leading-10 text-center text-white">ISACA</p>
+      </section>
     </>
   )
 }
