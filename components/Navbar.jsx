e3f3d7fa6a87fdc3f8f161b5bbc89ed4c20c384a
ManasPatil0967 <manaspatil0967@gmail.com>
2023-03-01 14:24:35 +0530

    Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="inline-flex w-screen space-x-24 items-center justify-center px-10 pt-16 pb-5">
+    <p className="w-20 text-sm font-medium tracking-wider leading-tight text-center text-white uppercase">Team</p>
+    <div className="w-20 h-20 h-full bg-white rounded-lg"></div>
+    <p className="w-20 text-sm font-medium tracking-wider leading-tight text-center text-white uppercase">SPONSOR</p>
+</div>
+  )
+}
+
+export default Navbar
